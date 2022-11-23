85f789920b64d4e43ac43ec5b56bf5b097b00c01
eliransc <scherzter@gmail.com>
2022-11-23 17:43:40 +0200

    prepare_data.py: creating training data

--- rnn-lstm-gru/sbatch_niagara_Data_generation_transient.sh
+++ rnn-lstm-gru/sbatch_niagara_Data_generation_transient.sh
@@ -2,6 +2,6 @@
 #SBATCH -t 0-23:58
 #SBATCH -A def-dkrass
 source /home/d/dkrass/eliransc/queues/bin/activate
-python /home/d/dkrass/eliransc/RNN_queue/rnn-lstm-gru/time_dependent.py
+python /scratch/d/dkrass/eliransc/RNN_queue/rnn-lstm-gru/time_dependent.py
 
 
