4d043e42f7ef165e33f7b8ce33376d548ab1097e
eliransc <scherzter@gmail.com>
2023-01-23 08:34:56 +0200

    prepare_data.py: creating training data

--- rnn-lstm-gru/sbatch_beluga_Gt_Gt_1_training.sh
+++ rnn-lstm-gru/sbatch_beluga_Gt_Gt_1_training.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -t 0-23:58
+#SBATCH -t 0-10:58
 #SBATCH -A def-dkrass
 #SBATCH --gpus-per-node=1
-#SBATCH --mem-per-cpu=10000M
+#SBATCH --mem-per-cpu=40000M
 source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
 python /home/eliransc/projects/def-dkrass/eliransc/RNN_queue/rnn-lstm-gru/train_GtGt1.py
 
