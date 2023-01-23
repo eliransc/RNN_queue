2824cd7e4c76ba9f9f9af2df226ece56d99ba64a
eliransc <scherzter@gmail.com>
2023-01-23 17:08:21 +0200

    prepare_data.py: creating training data

--- rnn-lstm-gru/train_GtGt1.py
+++ rnn-lstm-gru/train_GtGt1.py
@@ -19,7 +19,7 @@ import sys
 from datetime import datetime
 
 time_ub = 60
-num_moments = 4
+num_moments = 2
 class my_Dataset(Dataset):
     # Characterizes a dataset for PyTorch
     def __init__(self, data_paths):
