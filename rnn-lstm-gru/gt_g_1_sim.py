6cb2b3c360af5eb0744f0676b3fa65f957565bff
eliransc <scherzter@gmail.com>
2023-09-06 15:44:47 +0300

    prepare_data.py: creating training data

--- rnn-lstm-gru/gt_g_1_sim.py
+++ rnn-lstm-gru/gt_g_1_sim.py
@@ -1795,7 +1795,7 @@ def main(args):
     elif 'C:' in os.getcwd().split('/')[0]:
         args.read_path = r'C:\Users\user\workspace\data\mt_g_1'
     else:
-        args.read_path = '/scratch/eliransc/gt_g_1_data1' #
+        args.read_path = '/scratch/eliransc/gt_g_1_data2' #
 
     for ind in tqdm(range(args.num_iterations)):
 
