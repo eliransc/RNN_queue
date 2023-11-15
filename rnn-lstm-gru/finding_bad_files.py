6e323aaa0613a8421faa7b795926c0aa0d80b252
eliransc <scherzter@gmail.com>
2023-11-15 10:23:04 +0200

    prepare_data.py: creating training data

--- rnn-lstm-gru/finding_bad_files.py
+++ rnn-lstm-gru/finding_bad_files.py
@@ -10,9 +10,12 @@ path = '/scratch/eliransc/CSV4_experiment'
 files = os.listdir(path)
 
 
-for file in files:
-    try:
-        a, b = pkl.load(open(os.path.join(path, file)))
-    except:
-        print(file)
-        os.remove(os.path.join(path, file))
+for file in tqdm(files):
+    a, b = pkl.load(open(os.path.join(path, file)))
+
+
+    # try:
+    #     a, b = pkl.load(open(os.path.join(path, file)))
+    # except:
+    #     print(file)
+    #     os.remove(os.path.join(path, file))
