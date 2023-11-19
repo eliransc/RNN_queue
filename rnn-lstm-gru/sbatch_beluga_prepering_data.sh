75e5b545aa12e19261ea1e54d8a2e59231f7afa2
eliransc <scherzter@gmail.com>
2023-11-19 11:55:53 +0200

    prepare_data.py: creating training data

--- rnn-lstm-gru/sbatch_beluga_prepering_data.sh
+++ rnn-lstm-gru/sbatch_beluga_prepering_data.sh
@@ -3,6 +3,6 @@
 #SBATCH -A def-dkrass
 #SBATCH --mem 10000
 source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/RNN_queue/rnn-lstm-gru/prepering_data_RNN1.py
+python /home/eliransc/projects/def-dkrass/eliransc/RNN_queue/rnn-lstm-gru/prepering_data_RNN.py
 
 
