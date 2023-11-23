1048a7968c58f104d603daec62599cddfa207a0a
eliransc <scherzter@gmail.com>
2023-11-23 13:07:34 +0200

    prepare_data.py: creating training data

--- rnn-lstm-gru/sbatch_training.sh
+++ rnn-lstm-gru/sbatch_training.sh
@@ -4,6 +4,6 @@
 #SBATCH --mem 30000
 #SBATCH --gpus-per-node=1
 source /home/eliransc/projects/def-dkrass/eliransc/queues/bin/activate
-python /home/eliransc/projects/def-dkrass/eliransc/Deep_queue/code/training_rnn.py
+python /home/eliransc/projects/def-dkrass/eliransc/RNN_queue/rnn-lstm-gru/training_rnn.py
 
 
