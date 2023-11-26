40746bc544ba91fdb1189067c93d9bb656bef2b9
eliransc <scherzter@gmail.com>
2023-11-26 10:53:50 +0200

    prepare_data.py: creating training data

--- rnn-lstm-gru/training_rnn.py
+++ rnn-lstm-gru/training_rnn.py
@@ -76,7 +76,7 @@ def SAE_batch(soft, labels):
 
 
 def loss_queue1(soft, labels):
-    return ( (torch.abs(soft[:, :3, :] - labels[:, :3, :])).sum(axis=[2])).mean() + (
+    return ( (torch.abs(soft[:, :6, :] - labels[:, :6, :])).sum(axis=[2])).mean() + (
                 (torch.abs(soft[:, :, :] - labels[:, :, :])).sum(axis=[2]) +
                 torch.max(torch.abs(soft[:, :, :] - labels[:, :, :]), axis=2)[0]).mean()
 
